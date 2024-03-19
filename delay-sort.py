0ed8c357ec55d679a3b98f6431af4ced6bbc3d55
yuta3003 <y.nakazawa3003+github@gmail.com>
2024-03-19 17:11:37 +0900

    :+1: initial commit

---
+++ delay-sort.py
@@ -0,0 +1,46 @@
+import random
+import threading
+import time
+from typing import List
+
+
+def create_random_array(array_length: int) -> List[int]:
+    rand_array = [random.randint(0, 30) for _ in range(array_length)]
+    return rand_array
+
+
+def delay_sort(random_array: List[int]) -> List[int]:
+    sorted_array = []
+    array_lock = threading.Lock()
+
+    def _sleep_and_add(delay: int):
+        time.sleep(delay / 100)
+        with array_lock:
+            sorted_array.append(delay)
+
+    threads = []
+    for random_num in random_array:
+        thread = threading.Thread(target=_sleep_and_add, args=(random_num,))
+        threads.append(thread)
+        thread.start()
+
+    for thread in threads:
+        thread.join()
+
+    return sorted_array
+
+
+if __name__ == "__main__":
+
+    ARRAY_LENGTH = 10
+    random_array = create_random_array(ARRAY_LENGTH)
+    print(random_array)
+
+    print("")
+    print("")
+    print("    Delay-Sort start ")
+    print("")
+    print("")
+
+    sorted_array = delay_sort(random_array)
+    print(sorted_array)
